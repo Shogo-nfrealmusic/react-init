db246b9ff7dfa02298ea3f4eee44edfaa37948cf
Shogo-nfrealmusic <shogo.baseball0306@gmail.com>
2024-04-04 17:22:03 +0900

    progate react

--- src/App.js
+++ src/App.js
@@ -40,3 +40,9 @@ const handleAddTodo = () => {
 }
 
 export default App;
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import App from './components/App';
+
+// ReactDOM.render(<App />, document.getElementById('root'));
